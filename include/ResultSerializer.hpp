7d92d7ef5f3489a493f5222d9b1d6cc85adb0e23
Alper Ak <alperyasinak1@gmail.com>
2025-09-29 17:17:46 +0300

    Add ResultSerializer static utility class for converting detections to target format
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- include/ResultSerializer.hpp
+++ include/ResultSerializer.hpp
@@ -1,13 +1,21 @@
 #ifndef RESULTSERIALIZER_HPP_
 #define RESULTSERIALIZER_HPP_
 
+#include <InferenceEngine.hpp>
+#include <nlohmann/json.hpp>
+
 class ResultSerializer {
 public:
-    ResultSerializer();
-    ~ResultSerializer();
+    ResultSerializer() = delete;
+    ~ResultSerializer() = delete;
+    ResultSerializer(const ResultSerializer&) = delete;
+    ResultSerializer& operator=(const ResultSerializer&) = delete;
+    ResultSerializer(ResultSerializer&&) = delete;
+    ResultSerializer& operator=(ResultSerializer&&) = delete;
+
+    static nlohmann::json toJson(const std::vector<Detection>& detections);
 
 private:
-
 };
 
 #endif
--- src/ResultSerializer.cpp
+++ src/ResultSerializer.cpp
@@ -1,9 +1,23 @@
 #include "ResultSerializer.hpp"
 
-ResultSerializer::ResultSerializer() {
+nlohmann::json toJson(const std::vector<Detection>& detections)
+{
+    nlohmann::json detectionsJson;
+    detectionsJson["detections"] = nlohmann::json::array();
 
-}
+    for (const auto& detection : detections) {
+        detectionsJson["detections"].push_back({
+            {"classId", detection.classId},
+            {"label", detection.className},
+            {"confidence", detection.confidence},
+            {"boundingBox", {
+                {"x", detection.boundingBox.x},
+                {"y", detection.boundingBox.y},
+                {"width", detection.boundingBox.width},
+                {"height", detection.boundingBox.height}
+            }}
+        });
+    }
 
-ResultSerializer::~ResultSerializer() {
-
+    return detectionsJson;
 }

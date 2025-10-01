5426ab7f45d0e5c1bcc452baca58f2759a5b988e
Alper Ak <alperyasinak1@gmail.com>
2025-10-01 15:34:40 +0300

    Add PipelineController class to orchestrate entire processing pipeline
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- include/PipelineController.hpp
+++ include/PipelineController.hpp
@@ -1,13 +1,27 @@
 #ifndef PIPELINECONTROLLER_HPP_
 #define PIPELINECONTROLLER_HPP_
 
+#include "FrameHandler.hpp"
+#include "InferenceEngine.hpp"
+#include "FrameDecoder.hpp"
+#include "ResultSerializer.hpp"
+
 class PipelineController {
 public:
     PipelineController();
     ~PipelineController();
 
-private:
+    void start();
+    void stop();
 
+private:
+    void process();
+
+    std::thread pipelineThread_;
+    std::atomic<bool> isRunning_{false};
+
+    FrameHandler frameHandler_;
+    InferenceEngine inferenceEngine_;
 };
 
 #endif
--- src/PipelineController.cpp
+++ src/PipelineController.cpp
@@ -1,9 +1,54 @@
 #include "PipelineController.hpp"
 
-PipelineController::PipelineController() {
+PipelineController::PipelineController() :
+        frameHandler_{},
+        inferenceEngine_{"../model/yolo11s.onnx", cv::Size(640, 640), "../model/labels.txt",
+                        { .modelScoreThreshold = 0.45f, .modelNMSThreshold = 0.50f },
+                        InferenceTarget::GPU}
+{
 
 }
 
-PipelineController::~PipelineController() {
+PipelineController::~PipelineController()
+{
+    PipelineController::stop();
+}
 
+void PipelineController::start()
+{
+    if (isRunning_) {
+        return;
+    }
+
+    frameHandler_.start();
+    inferenceEngine_.start();
+
+    isRunning_ = true;
+    pipelineThread_ = std::thread(&PipelineController::process, this);
+}
+
+void PipelineController::stop()
+{
+    isRunning_ = false;
+
+    if (pipelineThread_.joinable()) {
+        pipelineThread_.join();
+    }
+
+    inferenceEngine_.stop();
+    frameHandler_.stop();
+}
+
+void PipelineController::process()
+{
+    while (isRunning_) {
+        auto encodedFrame = frameHandler_.getLatestFrame();
+        if (encodedFrame && !encodedFrame->empty()) {
+            auto decodedFrame = FrameDecoder::decodeJPEG(*encodedFrame);
+            inferenceEngine_.pushFrame(std::move(decodedFrame));
+            auto detections = inferenceEngine_.getDetections();
+            auto detectionsAsJson = ResultSerializer::toJson(detections);
+            frameHandler_.setFrameResult(detectionsAsJson);
+        }
+    }
 }

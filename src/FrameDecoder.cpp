7229e5e1b1d61e4c5bd9e925c7466ec0cd42baac
Alper Ak <alperyasinak1@gmail.com>
2026-01-31 14:38:54 +0300

    FrameDecoder: Make header-only and return std::optional
    - Replace empty cv::Mat with std::optional<cv::Mat> for decode failures
    - Make FrameDecoder a stateless header-only utility
    
    Signed-off-by: Alper Ak <alperyasinak1@gmail.com>

--- src/FrameDecoder.cpp
+++
@@ -1,9 +0,0 @@
-#include "FrameDecoder.hpp"
-
-cv::Mat FrameDecoder::decodeJPEG(const std::vector<uchar>& encodedFrame)
-{
-    if (encodedFrame.empty()) {
-        return {};
-    }
-    return cv::imdecode(encodedFrame, cv::IMREAD_COLOR);
-}
